1f4740736d8da559dcaf07d5033241afda28ff65
Hormic-Alem <developer.apoloalem@outlook.com>
2021-06-13 15:54:18 -0500

    Initial commit

---
+++ src/Coin.js
@@ -0,0 +1,26 @@
+import { symbol } from 'prop-types';
+import React from 'react';
+import './Coin.css';
+import './App.css';
+
+const Coin = ({name, image, symbol, price, volume, marketcap}) => {
+    return(
+        <div className="coin-container">
+         <div className="coin-row">
+            <div className="coin">
+            <img src={image} alt="crypto"/>
+            <h1>{name}</h1>
+            <p className="coin-symbol">{symbol}</p>
+            </div>
+            <div className="coin-data">
+                <p className="coin-price">${price}</p>
+                <p className="coin-volume">${volume.toLocaleString()}</p>
+                <p className="coin-marketcap">
+                    Mkt Cap: ${marketcap.toLocaleString()}
+                </p>
+            </div>
+         </div>
+        </div>
+    )
+}
+export default Coin;
